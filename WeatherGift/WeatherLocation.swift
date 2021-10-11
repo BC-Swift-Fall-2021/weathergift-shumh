7084926d82889cac6841d6a3cde5107f81c29bb6
Richard Shum <richard18shum@gmail.com>
2021-10-11 01:48:33 -0400

    6.2 WeatherLocations List Table View

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Richard Shum on 10/10/21.
+//
+
+import Foundation
+
+struct WeatherLocation{
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}

afe3bbd1c8677e15f7aa38f36d337cf2b7bda1c7
Richard Shum <richard18shum@gmail.com>
2021-10-11 22:05:13 -0400

    6.10 Using URL session to get JSON data from an API

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -17,4 +17,36 @@ class WeatherLocation: Codable{
         self.latitude = latitude
         self.longitude = longitude
     }
+    
+    func getData() {
+        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIkeys.openWeatherKey)"
+        
+        print("We are access the URL \(urlString)")
+        
+        // Create a URL
+        guard let url = URL(string: urlString) else {
+            print("Error: Could not create an URL from \(urlString)")
+            return
+        }
+        
+        // Create Session
+        let session = URLSession.shared
+        
+        // Get Data from .dataTask method
+        let task = session.dataTask(with: url) { data, reponse, error in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            
+            // deal with the data
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print("\(json)")
+            } catch {
+                print("JSON ERROR: \(error.localizedDescription)")
+            }
+        }
+        
+        task.resume()
+    }
 }
